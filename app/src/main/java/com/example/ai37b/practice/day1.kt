d858c8b9f9c1fae6e0ed4c19221ef586744833c7
ishan <ishanbashyal11@gmail.com>
2025-11-04 14:34:06 +0545

    Practice

---
+++ app/src/main/java/com/example/ai37b/practice/day1.kt
@@ -0,0 +1,67 @@
+package com.example.ai37b.practice
+
+// BASIC
+//fun main(){
+////    println("Hello")
+////    print("world")
+//    /*
+//    mutable:
+//        var name : String = "Pop"
+//        var name = "Pop"
+//        var name : Any = "Pop"
+//     */
+//    var name : String = "Pop"
+//    name = "boy"
+//    /*
+//    immutable:
+//        val age : Int = 15
+//     */
+//
+//    val age : Int = 15
+//
+//    println("My name is " + name + " and age is " + age)
+//    println("My name is $name and age is $age")
+//    println("My name is ${name.uppercase()} and age is $age")
+//}
+
+//Array
+//fun main(){
+//    val age = arrayOf(10,20,30)
+//    age[2] = 50
+//    println(age[2])
+//
+////    val age1 = ArrayList<Int>(10,20,30)
+//    val age1 = ArrayList<Int>()
+//    val age2 = arrayListOf<Int>(10,20,30)
+//
+//    age1.add(5)
+//    age2.add(10)
+//
+//}
+
+
+//FOR
+//fun main(){
+//    for(i in 0 .. 10){
+//        println(i)
+//    }
+//    for(i in 0 .. 10 step 2){
+//        println(i)
+//    }
+//    for(i in 0 until 10 step 2){
+//        println(i)
+//    }
+//    for(i in 10 downTo 0 step -1){
+//        println(i)
+//    }
+//}
+
+
+//Functions
+//fun sum(a:Int, b:Int) : Int {
+//    return a+b;
+//}
+//
+//fun login(username:String, password:String) :Unit{
+//    //unit is void
+//}
---
+++ app/src/main/java/com/example/ai37b/practice/var_use.kt
@@ -0,0 +1,10 @@
+package com.example.ai37b.practice
+
+//mutable
+//we can re assign values to mutable variables
+var age = 20;
+
+
+//immutable
+//we can't re assign values to immutable variables
+val roll = 15
